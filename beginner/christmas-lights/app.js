a51236574b52a7615014ffe9099d34f7db5d4c24
Miguel Duque <mduquem1@gmail.com>
2020-03-05 16:27:19 -0500

    Add Christmas lights

---
+++ beginner/christmas-lights/app.js
@@ -0,0 +1,25 @@
+document.getElementById('start').addEventListener('click', start);
+document.getElementById('stop').addEventListener('click', stop);
+function start() {
+   const dataLight = document.querySelectorAll('#light');
+   console.log(dataLight);
+   dataLight.forEach(function(item) {
+      item.classList.add('light');
+   });
+   const dataNormal = document.querySelectorAll('#normal');
+   dataNormal.forEach(function(item) {
+      item.classList.add('normal');
+   });
+}
+
+function stop() {
+   const dataLight = document.querySelectorAll('#light');
+   console.log(dataLight);
+   dataLight.forEach(function(item) {
+      item.classList.remove('light');
+   });
+   const dataNormal = document.querySelectorAll('#normal');
+   dataNormal.forEach(function(item) {
+      item.classList.remove('normal');
+   });
+}
